992f1034a80ecb6dc0c0d3cd59bfc453b99580e2
vklochkov <Viktor.Klochkov@kaspi.kz>
2025-03-02 18:21:13 +0500

    Add channel registration; implement command handling for Telegram bot and update user model. TelegramUser key type changed

--- src/main/java/com/github/viktor235/schedalertbot/telegram/TelegramUser.java
+++ src/main/java/com/github/viktor235/schedalertbot/telegram/TelegramUser.java
@@ -3,27 +3,23 @@ package com.github.viktor235.schedalertbot.telegram;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-
-import java.util.List;
 
 @Document(collection = "users")
 @Getter
 @Setter
 @Builder
+@ToString
 public class TelegramUser {
     //todo audit
 
     @Id
-    private long id;
+    private String id;
     @Field
     private String username;
     @Field
-    private boolean isAdmin;
-    @Field
-    private String timezone;
-    @Field
-    private List<String> sites;
+    private String channelId;
 }
